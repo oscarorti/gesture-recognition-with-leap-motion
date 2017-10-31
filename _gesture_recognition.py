68985f6cc131115b4628aa467b7b9f98b97caf16
Òscar Orti Gonzalez <oscarortigonzalez@Oscars-MacBook-Pro.local>
2017-10-31 12:19:56 +0100

    Add minimal GUI (with errrors), rename scripts and some new functionalities in swipe detection

--- _gesture_recognition.py
+++ _gesture_recognition.py
@@ -6,37 +6,42 @@
 # between Leap Motion and you, your company or other organization.             #
 ################################################################################
 
-import os, sys, inspect, thread, time
+import inspect
+# Import native python libraries
+import os
+import sys
+
+# Import project modules
+
+# Setup environment variables
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+
 # Windows and Linux
 arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
+
 # Mac
-#arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
+# arch_dir = os.path.abspath(os.path.join(src_dir, '../lib')
 
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
+# Import external libraries
 import Leap
-from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
-import classListener
+from _plotter import Plotter
+import _classListener
 
 
 def main():
     # Create a sample listener and controller
-    listener = classListener.MyListener()
+    listener = _classListener.MyListener()
     controller = Leap.Controller()
+
 
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
+    plotter = Plotter(listener)
 
-    # Keep this process running until Enter is pressed
-    print "Press Enter to quit..."
-    try:
-        sys.stdin.readline()
-    except KeyboardInterrupt:
+    while True:
         pass
-    finally:
-        # Remove the sample listener when done
-        controller.remove_listener(listener)
 
 
 if __name__ == "__main__":
